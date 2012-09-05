6a3c838af230d793f5c1e1c99f3011af33a7bfeb
Urabe, Shyouhei <shyouhei@ruby-lang.org>
2012-09-06 01:04:20 +0900

    License terms update

--- ext/open_temporary/open_temporary.c
+++ ext/open_temporary/open_temporary.c
@@ -1,26 +1,26 @@
-/*
- * open-temporary is Copyright(c) 2012 Urabe, Shyouhei.  All rights Reserved.
+/* Copyright (c) 2012 Urabe, Shyouhei
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of  this software and  associated documentation  files (the  "Software"), to
- * deal in  the software without restriction, including  without limitation the
+ * deal in  the Software without restriction, including  without limitation the
  * rights to use, copy,  modify, merge, publish, distribute, sublicense, and/or
- * sell copies of  the software, and to permit persons to  whom the software is
- * furnished to do so, subject to the following condition:
+ * sell copies of  the Software, and to permit persons to  whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * 1. The above  copyright notice and this permission  notice shall be included
- *    in all copies or substantial portions of the software.
+ *   The above copyright notice and this permission notice shall be included
+ *   in all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS  PROVIDED "AS IS", WITHOUT WARRANTY OF  ANY KIND, EXPRESS OR
  * IMPLIED,  INCLUDING BUT NOT  LIMITED TO  THE WARRANTIES  OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
- * AUTHOR  OR COPYRIGHT  HOLDERS  BE LIABLE  FOR  ANY CLAIM,  DAMAGES OR  OTHER
+ * AUTHORS  OR COPYRIGHT  HOLDERS BE  LIABLE FOR  ANY CLAIM,  DAMAGES  OR OTHER
  * LIABILITY,  WHETHER IN  AN ACTION  OF CONTRACT,  TORT OR  OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
 
 #include <ruby.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 static VALUE tmpdir = Qundef;
