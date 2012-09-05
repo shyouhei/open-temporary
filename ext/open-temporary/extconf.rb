f033173c9a76daf55e54341ab9f4a299bc6c2ac5
Urabe, Shyouhei <shyouhei@ruby-lang.org>
2012-09-05 23:20:00 +0900

    echo create_makefile 'open-temporary' > ext/open-temporay/extconf.rb

---
+++ ext/open-temporary/extconf.rb
@@ -0,0 +1,13 @@
+require 'mkmf'
+create_makefile 'open-temporary'
+
+# 
+# Local Variables:
+# mode: ruby
+# coding: utf-8-unix
+# indent-tabs-mode: t
+# tab-width: 3
+# ruby-indent-level: 3
+# fill-column: 79
+# default-justification: full
+# End:
